c7047800b0b33fc68633ae81116d7e5103234fb1
Michel Nsimba Matondo <nsimba_matondo@yahoo.fr>
2020-07-06 20:49:50 +0200

    Adding the ICategoryRepository interface

---
+++ WalloniaPastryShop/Repository/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WalloniaPastryShop.Models;
+
+namespace WalloniaPastryShop.Repository
+{
+    public interface ICategoryRepository
+    {
+        public IEnumerable<Category> AllCategories { get; }
+    }
+}

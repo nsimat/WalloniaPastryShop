62eeb545d22b7f42dfb3ad98d4f31226e42f8b77
Michel Nsimba Matondo <nsimba_matondo@yahoo.fr>
2020-07-07 16:27:54 +0200

    Adding the PieController

---
+++ WalloniaPastryShop/Controllers/PieController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WalloniaPastryShop.Models;
+using WalloniaPastryShop.Repository;
+
+namespace WalloniaPastryShop.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository pieRepository;
+        private readonly ICategoryRepository categoryRepository;
+
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            this.pieRepository = pieRepository;
+            this.categoryRepository = categoryRepository;
+        }
+
+        public ViewResult List()
+        {
+            return View(pieRepository.AllPies);
+        }
+    }
+}

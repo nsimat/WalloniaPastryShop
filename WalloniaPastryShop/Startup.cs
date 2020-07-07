392913321002f5c82da997fa7fb9a6b989daaca2
Michel Nsimba Matondo <nsimba_matondo@yahoo.fr>
2020-07-07 15:55:48 +0200

    Configuring controllers routes and dependency injection for repository classes

--- WalloniaPastryShop/Startup.cs
+++ WalloniaPastryShop/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using WalloniaPastryShop.Repository;
 
 namespace WalloniaPastryShop
 {
@@ -16,6 +17,9 @@ namespace WalloniaPastryShop
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<IPieRepository, MockPieRepository>();
+            services.AddScoped<ICategoryRepository, MockCategoryRepository>();
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,10 +34,9 @@ namespace WalloniaPastryShop
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }

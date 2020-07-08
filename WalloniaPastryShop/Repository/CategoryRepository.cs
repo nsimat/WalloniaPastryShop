1b22e0975939c9b2db7184505ed0c84b0dae6ed2
Michel Nsimba Matondo <nsimba_matondo@yahoo.fr>
2020-07-08 22:12:52 +0200

    Adding CategoryRepository to implement ICategoryrepository

---
+++ WalloniaPastryShop/Repository/CategoryRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WalloniaPastryShop.Models;
+
+namespace WalloniaPastryShop.Repository
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly WalloniaPastryShopDbContext _walloniaPastryShopDbContext;
+
+        public CategoryRepository(WalloniaPastryShopDbContext walloniaPastryShopDbContext)
+        {
+            _walloniaPastryShopDbContext = walloniaPastryShopDbContext;
+        }
+        public IEnumerable<Category> AllCategories => _walloniaPastryShopDbContext.Categories;
+    }
+}

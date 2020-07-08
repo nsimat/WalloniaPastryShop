363ccfc2c4fb495c5a92e0cf412237fce6eb420f
Michel Nsimba Matondo <nsimba_matondo@yahoo.fr>
2020-07-08 22:06:10 +0200

    Implementing the IPieRepository by PieRepository class

---
+++ WalloniaPastryShop/Repository/PieRepository.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WalloniaPastryShop.Models;
+
+namespace WalloniaPastryShop.Repository
+{
+    public class PieRepository : IPieRepository
+    {
+        private readonly WalloniaPastryShopDbContext _walloniaPastryShopDbContext;
+
+        public PieRepository(WalloniaPastryShopDbContext walloniaPastryShopDbContext)
+        {
+            _walloniaPastryShopDbContext = walloniaPastryShopDbContext;
+        }
+        public IEnumerable<Pie> AllPies 
+        {
+            get
+            {
+                return _walloniaPastryShopDbContext.Pies.Include(c => c.Category);
+            }
+        }
+
+        public IEnumerable<Pie> PiesOfTheWeek
+        {
+            get
+            {
+                return _walloniaPastryShopDbContext.Pies.Include(c => c.Category).Where(p => p.IsPieOfTheWeek);
+            }
+        }
+
+        public Pie GetPieById(int pieId)
+        {
+            return _walloniaPastryShopDbContext.Pies.FirstOrDefault(p => p.PieId == pieId);
+        }
+    }
+}
--- WalloniaPastryShop/Startup.cs
+++ WalloniaPastryShop/Startup.cs
@@ -5,18 +5,33 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using WalloniaPastryShop.Models;
 using WalloniaPastryShop.Repository;
 
 namespace WalloniaPastryShop
 {
     public class Startup
     {
+        public IConfiguration  Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<WalloniaPastryShopDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            });
+
             services.AddScoped<IPieRepository, MockPieRepository>();
             services.AddScoped<ICategoryRepository, MockCategoryRepository>();
             services.AddControllersWithViews();

6a4837dfd00044d66a530af85523361ec52fe2d9
Michel Nsimba Matondo <nsimba_matondo@yahoo.fr>
2020-07-13 00:21:09 +0200

    Adding the HomeController

---
+++ WalloniaPastryShop/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WalloniaPastryShop.Repository;
+using WalloniaPastryShop.ViewModels;
+
+namespace WalloniaPastryShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+
+        public HomeController(IPieRepository pieRepository)
+        {
+            _pieRepository = pieRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var homeViewModel = new HomeViewModel
+            {
+                PiesOfTheWeek = _pieRepository.PiesOfTheWeek
+            };
+
+            return View(homeViewModel);
+        }
+    }
+}
---
+++ WalloniaPastryShop/ViewModels/HomeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WalloniaPastryShop.Models;
+
+namespace WalloniaPastryShop.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Pie> PiesOfTheWeek { get; set; }
+    }
+}

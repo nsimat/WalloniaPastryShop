a83097aba79b9cbac09d638b500f9242ea6e7276
Michel Nsimba Matondo <nsimba_matondo@yahoo.fr>
2020-07-19 01:26:11 +0200

    Adding Order and OrderDetail classes to Models

---
+++ WalloniaPastryShop/Models/Order.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WalloniaPastryShop.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public List<OrderDetail> OrderDetails { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string PostalCode { get; set; }
+        public string City { get; set; }
+        public string Region { get; set; }
+        public string Country { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public decimal OrderTotal { get; set; }
+        public DateTime OrderPlaced { get; set; }
+    }
+}
